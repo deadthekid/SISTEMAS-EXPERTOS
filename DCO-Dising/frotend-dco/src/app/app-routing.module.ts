30126c1e332704b482525799388eced73a337315
Heber Meza <hebermeza0@gmail.com>
2022-07-20 20:04:02 -0600

    avance usuarios-principal

--- DCO-Dising/frotend-dco/src/app/app-routing.module.ts
+++ DCO-Dising/frotend-dco/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalUsuariosComponent } from './components/Usuarios/principal-usuarios/principal-usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path :'usuarios', component:PrincipalUsuariosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- DCO-Dising/frotend-dco/src/app/app.module.ts
+++ DCO-Dising/frotend-dco/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalUsuariosComponent } from './components/Usuarios/principal-usuarios/principal-usuarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalUsuariosComponent
   ],
   imports: [
     BrowserModule,
