cc9287387aa8f6d919489d97238574d45cf33040
Heber Meza <hebermeza0@gmail.com>
2022-07-21 14:37:49 -0600

    html ver-producto terminada

--- DCO-Dising/frotend-dco/src/app/app-routing.module.ts
+++ DCO-Dising/frotend-dco/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrincipalUsuariosComponent } from './components/Usuarios/principal-usuarios/principal-usuarios.component';
+import { VerProductoComponent } from './components/Usuarios/ver-producto/ver-producto.component';
 
 const routes: Routes = [
-  {path :'usuarios', component:PrincipalUsuariosComponent}
+  {path :'usuarios', component:PrincipalUsuariosComponent},
+  {path :'usuarios/verProducto', component:VerProductoComponent}
 ];
 
 @NgModule({
--- DCO-Dising/frotend-dco/src/app/app.module.ts
+++ DCO-Dising/frotend-dco/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrincipalUsuariosComponent } from './components/Usuarios/principal-usuarios/principal-usuarios.component';
+import { VerProductoComponent } from './components/Usuarios/ver-producto/ver-producto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrincipalUsuariosComponent
+    PrincipalUsuariosComponent,
+    VerProductoComponent
   ],
   imports: [
     BrowserModule,
