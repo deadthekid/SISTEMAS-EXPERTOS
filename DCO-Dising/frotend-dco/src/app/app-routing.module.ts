a6c0f8bff743485f48cd7d710c3b466b333ebe44
Heber Meza <hebermeza0@gmail.com>
2022-07-23 18:44:00 -0600

    registro usuario frontend terminado

--- DCO-Dising/frotend-dco/src/app/app-routing.module.ts
+++ DCO-Dising/frotend-dco/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreacionUsuarioComponent } from './components/Usuarios/creacion-usuario/creacion-usuario.component';
 import { PrincipalUsuariosComponent } from './components/Usuarios/principal-usuarios/principal-usuarios.component';
 import { VerProductoComponent } from './components/Usuarios/ver-producto/ver-producto.component';
 
 const routes: Routes = [
   {path :'usuarios', component:PrincipalUsuariosComponent},
-  {path :'usuarios/verProducto', component:VerProductoComponent}
+  {path :'usuarios/verProducto', component:VerProductoComponent},
+  {path :'usuarios/crearUsuario', component:CreacionUsuarioComponent}
 ];
 
 @NgModule({
--- DCO-Dising/frotend-dco/src/app/app.module.ts
+++ DCO-Dising/frotend-dco/src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { PrincipalUsuariosComponent } from './components/Usuarios/principal-usuarios/principal-usuarios.component';
 import { VerProductoComponent } from './components/Usuarios/ver-producto/ver-producto.component';
+import { CreacionUsuarioComponent } from './components/Usuarios/creacion-usuario/creacion-usuario.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     PrincipalUsuariosComponent,
-    VerProductoComponent
+    VerProductoComponent,
+    CreacionUsuarioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
+
   ],
   providers: [],
   bootstrap: [AppComponent]
