8029a3b8b81107af59b64bac86a037c1e3985e49
unknown <arielgmez@unah.hn>
2022-07-26 19:01:09 -0600

    componentes basicos administracion

--- DCO-Dising/frotend-dco/src/app/app.module.ts
+++ DCO-Dising/frotend-dco/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './components/administracion/login/login.component';
+import { HomeComponent } from './components/administracion/home/home.component';
+import { EmpresasComponent } from './components/administracion/empresas/empresas.component';
+import { UsuariosComponent } from './components/administracion/usuarios/usuarios.component';
+import { PlanesComponent } from './components/administracion/planes/planes.component';
+import { TemasComponent } from './components/administracion/temas/temas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    EmpresasComponent,
+    UsuariosComponent,
+    PlanesComponent,
+    TemasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
