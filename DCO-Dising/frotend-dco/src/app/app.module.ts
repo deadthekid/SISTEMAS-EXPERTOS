5d62dab48b75c69d3121d6cf5a7085ae1a352d3f
Ariel Armando Gómez Ramos <arielgmez@unah.hn>
2022-07-28 12:22:02 -0600

    Revert "Administracion"

--- DCO-Dising/frotend-dco/src/app/app.module.ts
+++ DCO-Dising/frotend-dco/src/app/app.module.ts
@@ -4,20 +4,6 @@ import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminLoginComponent } from './components/administracion/admin-login/admin-login.component';
-import { AdminHomeComponent } from './components/administracion/admin-home/admin-home.component';
-import { AdminEmpresasComponent } from './components/administracion/admin-empresas/admin-empresas.component';
-import { AdminUsuariosComponent } from './components/administracion/admin-usuarios/admin-usuarios.component';
-import { AdminPlanesComponent } from './components/administracion/admin-planes/admin-planes.component';
-import { AdminTemasComponent } from './components/administracion/admin-temas/admin-temas.component';
-import { AdminNavbarComponent } from './components/administracion/admin-navbar/admin-navbar.component';
-import { AdminDetallesUsuarioComponent } from './components/administracion/admin-detalles-usuario/admin-detalles-usuario.component';
-import { AdminDetallesEmpresaComponent } from './components/administracion/admin-detalles-empresa/admin-detalles-empresa.component';
-import { AdminDetallesPlanComponent } from './components/administracion/admin-detalles-plan/admin-detalles-plan.component';
-import { AdminDetallesTemaComponent } from './components/administracion/admin-detalles-tema/admin-detalles-tema.component';
-import { AdminNuevoUsuarioComponent } from './components/administracion/admin-nuevo-usuario/admin-nuevo-usuario.component';
-import { AdminNuevoPlanComponent } from './components/administracion/admin-nuevo-plan/admin-nuevo-plan.component';
-import { AdminNuevoTemaComponent } from './components/administracion/admin-nuevo-tema/admin-nuevo-tema.component';
 import { LoginComponent } from './components/empresa/login/login.component';
 import { RegistroComponent } from './components/empresa/registro/registro.component';
 import { PrincipalComponent } from './components/empresa/principal/principal.component';
@@ -44,20 +30,6 @@ import { PrincipalUsuariosAutenticadoComponent } from './components/Usuarios/pri
 @NgModule({
   declarations: [
     AppComponent,
-    AdminLoginComponent,
-    AdminHomeComponent,
-    AdminEmpresasComponent,
-    AdminUsuariosComponent,
-    AdminPlanesComponent,
-    AdminTemasComponent,
-    AdminNavbarComponent,
-    AdminDetallesUsuarioComponent,
-    AdminDetallesEmpresaComponent,
-    AdminDetallesPlanComponent,
-    AdminDetallesTemaComponent,
-    AdminNuevoUsuarioComponent,
-    AdminNuevoPlanComponent,
-    AdminNuevoTemaComponent,
     LoginComponent,
     RegistroComponent,
     PrincipalComponent,
@@ -78,10 +50,12 @@ import { PrincipalUsuariosAutenticadoComponent } from './components/Usuarios/pri
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxPaginationModule,
+    NgxPaginationModule
     ReactiveFormsModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot()
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
