0512270948a9d6ca8aaad55c1717195a03b66150
Ariel Armando Gómez Ramos <arielgmez@unah.hn>
2022-08-07 19:43:36 -0600

    admin login, frontend

--- DCO-Dising/frotend-dco/src/app/components/administracion/admin-login/admin-login.component.ts
+++ DCO-Dising/frotend-dco/src/app/components/administracion/admin-login/admin-login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AdminService } from 'src/app/services/admin.service';
 
 @Component({
   selector: 'app-admin-login',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminLoginComponent implements OnInit {
 
-  constructor() { }
+  formLogin =  new FormGroup({
+    correo: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+    contrasena: new FormControl('', [Validators.required])
+  })
+
+  constructor(
+    private router: Router,
+    private toastr: ToastrService,
+    private adminService : AdminService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    let login = this.formLogin.value;
+    this.adminService.login(login).subscribe(data => {
+      if(data.acceso){
+        console.log(data.usuario);
+        localStorage.setItem('usuarioAdmin', data.usuario._id);
+        this.toastr.success(data.mensaje, 'Bienvenido ' +data.usuario.nombre);
+        this.router.navigate(['/admin/home']);
+      }else{
+        this.toastr.error('Datos incorrectos', data.mensaje);
+      }
+    }, error =>{
+      console.log(error);
+    });
   }
 
 }
